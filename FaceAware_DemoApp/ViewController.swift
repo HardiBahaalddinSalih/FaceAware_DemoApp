1df8f159197fcc40fe1f23207820702481cb5c44
HardiB.Salih <>
2018-05-27 09:09:50 +0300

    first commit

---
+++ FaceAware_DemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceAware_DemoApp
+//
+//  Created by HardiB.Salih on 5/27/18.
+//  Copyright © 2018 HardiB.Salih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
